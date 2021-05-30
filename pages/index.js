c4537257d7e039a21bb91adb6e1f5c2919b31793
Adrian Vendrametto <adrianvendrametto@gmail.com>
2021-05-30 18:15:54 -0300

    versão com html

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function Home(){
-    return <div> Home - VdTech </div>
-}
-
-export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+function Sobre(){
+    return (
+        <div> <h1>Home - VdTech</h1>
+        <Link href='/home'>
+            <a>Acessar Página home</a>
+        </Link>
+    </div>)
+}
+
+//pesquisar essa função export default do NEXT JS.
+export default Sobre
